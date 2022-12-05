8fc14f46a6751714f0066082efffafd3d47a1dad
Daniel Brož <danobroz66@gmail.com>
2022-12-05 15:45:02 +0100

    add colors and container settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        '2xl': '1040px',
+      },
+    },
+    extend: {
+      colors: {
+        Blue: {
+          50: '#E5F1FF',
+          100: '#CCE4FF',
+          200: '#99C9FF',
+          300: '#66ADFF',
+          400: '#3392FF',
+          500: '#60AAFF',
+          600: '#005FCC',
+          700: '#004799',
+          800: '#003066',
+          900: '#001833',
+        },
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 }
